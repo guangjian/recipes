fa967845fdefd20a016f008bd02f3010aff72601
MitchellGerdisch <mitchell.gerdisch@alcatel-lucent.com>
2012-11-28 15:10:39 -0600

    bug fix for nfs persmissions

--- apps/ownCloud/nfsServer/nfsServer_install.sh
+++ apps/ownCloud/nfsServer/nfsServer_install.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 # install nfs bits and make things so they start at boot
-yum install -y nfs-utils
+# also install httpd so that we have an apache user in place.
+yum install -y nfs-utils httpd
 chkconfig rpcbind on
 chkconfig nfslock on
 chkconfig nfs on
 
 # make a directory and make it available as an NFS directory
 mkdir /opt/owncloudshare
+chown -R apache:apache /opt/owncloudshare
+chmod 0770 /opt/owncloushare
 echo "/opt/owncloudshare 135.0.0.0/8(rw,no_root_squash,sync)" >> /etc/exports
