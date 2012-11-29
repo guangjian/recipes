ec355e6f36684c7a3a3a1d7f94685c13dd6a35f8
MitchellGerdisch <mitchell.gerdisch@alcatel-lucent.com>
2012-11-29 13:12:56 -0600

    typo

--- apps/ownCloud/mysql/mysql-service.groovy
+++ apps/ownCloud/mysql/mysql-service.groovy
@@ -12,7 +12,6 @@
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 * See the License for the specific language governing permissions and
 * limitations under the License.
-* Modified by Mitch G
 *******************************************************************************/
 import groovy.sql.*
 import com.mysql.jdbc.Driver
