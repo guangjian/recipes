de5eef473ed37805f3baee868654cb28194ee0e4
BrandenH <branden.hughes@gmail.com>
2012-11-29 12:16:17 -0500

    Added debug statements

--- apps/ownCloud/ownCloudFE/owncloud_start.groovy
+++ apps/ownCloud/ownCloudFE/owncloud_start.groovy
@@ -1,34 +1,38 @@
 import org.hyperic.sigar.OperatingSystem
 import org.cloudifysource.usm.USMUtils
 import org.cloudifysource.dsl.context.ServiceContextFactory
-import  java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeUnit
 
 // Then here's the logic
 
 context = ServiceContextFactory.getServiceContext()
 
+hostIp=InetAddress.localHost.hostAddress
+println "HostIP is: $hostIP"
+
 def mysqlService = context.waitForService("mysql", 300, TimeUnit.SECONDS)
 mysqlHostInstances = mysqlService.waitForInstances(mysqlService.numberOfPlannedInstances, 60, TimeUnit.SECONDS)
-
-def nfsService = context.waitForService("NFS", 300, TimeUnit.SECONDS)
-nfsHostInstances = nfsService.waitForInstances(nfsService.numberOfPlannedInstances, 60, TimeUnit.SECONDS)
-
-hostIp=InetAddress.localHost.hostAddress
-
-// Logic for owncloud recipe to get IPs of MySQL and NFS servers:
 
 mysqlServerIP = mysqlHostInstances[0].hostAddress
 if (mysqlServerIP == null) {
         mysqlServerIP = hostIp
 }
 
+println "Retrieving mySqlServerIP: $mysqlServerIP"
+
+def nfsService = context.waitForService("NFS", 300, TimeUnit.SECONDS)
+nfsHostInstances = nfsService.waitForInstances(nfsService.numberOfPlannedInstances, 300, TimeUnit.SECONDS)
+
 nfsServerIP = nfsHostInstances[0].hostAddress
 if (nfsServerIP == null) {
         nfsServerIP = hostIp
 }
 
+println "Retrieving nfsServerIP: $nfsServerIP"
+
 Builder = new AntBuilder()
 
+println "Start: copy config and execute start.sh"
 Builder.sequential {
 		replaceregexp(file:"${context.serviceDirectory}/config.php",
 					match:"MYSQLSERVERIP",
@@ -37,4 +41,6 @@ Builder.sequential {
 		exec(executable:"${context.serviceDirectory}/start.sh",osfamily:"unix") {
 			arg value:"$NFSSERVERIP"
 			}
+println "Finished attempting to copy config and execute start.sh"
+
 }
