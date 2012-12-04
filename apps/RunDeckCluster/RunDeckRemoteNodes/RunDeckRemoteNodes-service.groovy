15e5d3517bc1704b149a51f69dda1302c2106d6e
MitchellGerdisch <mitchell.gerdisch@alcatel-lucent.com>
2012-12-03 18:54:34 -0600

    added imports

--- apps/RunDeckCluster/RunDeckRemoteNodes/RunDeckRemoteNodes-service.groovy
+++ apps/RunDeckCluster/RunDeckRemoteNodes/RunDeckRemoteNodes-service.groovy
@@ -1,3 +1,8 @@
+import java.util.concurrent.TimeUnit
+import org.cloudifysource.dsl.context.ServiceContextFactory
+import org.cloudifysource.dsl.utils.ServiceUtils;
+import org.hyperic.sigar.OperatingSystem
+
 /*
  * Version 1: Basic deployment. Cannot be scaled.
  * Version 2: Support manual scaling (i.e. via cloudify shell use set-instances RunDeckRemoteNodes X to set the number of instances wanted
