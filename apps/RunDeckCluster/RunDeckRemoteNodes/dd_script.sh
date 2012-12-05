d64031518987eba48a2314894c6f32693be3d633
MitchellGerdisch <mitchell.gerdisch@alcatel-lucent.com>
2012-12-05 12:51:08 -0600

    modified dd_script a bit

--- apps/RunDeckCluster/RunDeckRemoteNodes/dd_script.sh
+++ apps/RunDeckCluster/RunDeckRemoteNodes/dd_script.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 
-USAGE="usage: dd-test outfile\
-where outfile is the file to which the bytes should be written"
-# no error checking is done other than that outfile is specified.
+blocksize_list=4 8 32 128 256 512
+outfile="/tmp/ofile"
+USAGE="usage: dd-test [-o <outfile>] [-b <blocksize>]\
+-o: The outfile to which bytes should be written. File is deleted when done. If omitted, defaults to /tmp/ofile.\
+-b: The blocksize in KB to use. If omitted the script will run the test will run the dd for the blocksizes: \[${blocksize_list}\]"
 
-if [ $# -ne 1 ]
-then
-      echo "need an output file" >&2
-      echo "${USAGE}" >&2
-      exit 1
-fi
+while getopts o:b: param
+do
+	case "${param}" in
+		b)	blocksize_list="${OPTARG}"
+			;;
+		o)	outfile="${OPTARG}"
+			;;
+		*)	echo ${USAGE}
+			exit 1
+			;;
+	esac
+done
+
 
 # Get total system memory.  MULT is how many times total memory we want the files to be (should be > 1)
 # The icky command says to get the Mem line from free, cut off the numbers, strip leading white space,
@@ -21,10 +30,10 @@ MULT=2
 # Run through different block sizes (in Kbytes) - change the list as needed.
 # To figure count, assume we need MULT x physical RAM so count = MEM * MULT / bs.
 #
-for bs in 4 8 32 128 256 512
+for bs in ${blocksize_list}
 do
 	count=$(echo ${MEM}*${MULT}/${bs} | bc)
-	cmd="dd if=/dev/urandom of=$1 iflag=fullblock oflag=direct bs=${bs}K count=${count}"
+	cmd="dd if=/dev/urandom of=${outfile} iflag=fullblock oflag=direct bs=${bs}K count=${count}"
 	echo "cmd: ${cmd}"
 	eval "${cmd}"
     rm $1
