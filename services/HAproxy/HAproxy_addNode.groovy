d5f603fdb58552770b84d38d7497497ae798558b
MitchellGerdisch <mitchell.gerdisch@alcatel-lucent.com>
2012-12-13 12:21:04 -0600

    removed crap left in addNode

--- services/HAproxy/HAproxy_addNode.groovy
+++ services/HAproxy/HAproxy_addNode.groovy
@@ -29,33 +29,3 @@ Builder.sequential {
 	// Now add back in the line used to do the above substitution.
 	echo(message:"#INSERT BACKEND SERVER ENTRY HERE#\n", file:"${haproxy_cfg_dir}/${haproxy_cfg_file}", append:"true");
 }
-
-
-
-
-def dnsServerListFile = new File("${serverListFile}")
-def dnsServerListText = dnsServerListFile.text
-
-
-// Update the server list file on the dnsLoadGenerator instance if necessary.
-if ( dnsServerListText.contains(dnsVmIp)) {
-	println "addNode: Not adding ${dnsVmIp} to ${serverListFile} because it's already there..."	
-}
-else { 
-	def modifiedText = dnsServerListText.replace("${serverListPreamble}", "${serverListPreamble}" + System.getProperty("line.separator") + "${dnsVmIp}")				
-	dnsServerListFile.text = modifiedText
-	println "addNode: Added ${dnsVmIp} to ${serverListFile} text is now : ${modifiedText}..."
-
-/* NOT using this logic since I don't think I need to save the data as part of the application attributes.
- * But if I do decide I do want to, I'm keeping this logic handy
-
-	def balancerMembers=context.attributes.thisService["balancerMembers"]
-	if ( balancerMembers == null ) {
-		balancerMembers = ""
-	}
-	balancerMembers +=",${balancerMemberText},"										
-	context.attributes.thisService["balancerMembers"]=balancerMembers
-	println "addNode: Added ${node} to context balancerMembers which is now ${balancerMembers}"
-*/
-
-}
